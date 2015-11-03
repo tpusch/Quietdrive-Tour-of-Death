b7954b4b8845793cf65c1ea0a930556da83627aa
SkippyNumNums <lars2mat@live.bemidjistate.edu>
2015-11-02 21:35:57 -0600

    Coins and a "Bus"
    Added and animated gold and silver coins that are destroyed when the
    player collides with them. Would like to see them spawn from dead
    enemies and affect a player score eventually. (This would add some
    competitive aspects to the game.)
    
    Also added a "bus" (aka a couple blocks and a big collider) behind the
    player spawn point to prevent players from running off the map
    backwards.

---
+++ Assets/grab_coin.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grab_coin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        
+        /*if (coll.gameObject.tag == "Player")
+            Destroy(coll.gameObject);*/
+        if (coll.gameObject.tag == "coin")
+            Destroy(coll.gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+}
