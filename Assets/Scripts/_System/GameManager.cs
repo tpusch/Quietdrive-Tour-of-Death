978ac9a07696972da1f1210383f9e8dbf92a0b65
tpusch <tyler.puschinsky@gmail.com>
2015-11-04 12:17:54 -0600

    Programmatic background

--- Assets/Scripts/_System/GameManager.cs
+++ Assets/Scripts/_System/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour {
 
     [SerializeField]
     GameObject groundPrefab;
+    [SerializeField]
+    GameObject backgroundImage;
 
     [SerializeField]
     UnityStandardAssets._2D.PlatformerCharacter2D player1;
@@ -32,7 +34,7 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
-        BuildGround();
+        BuildLevel();
         numPlayers = PlayerPrefs.GetInt("NumPlayers", defaultPlayers);
         if (numPlayers == 1)
         {
@@ -45,13 +47,21 @@ public class GameManager : MonoBehaviour {
 	
 	}
 
-    void BuildGround()
+    void BuildLevel()
     {
+        //ground
         GameObject groundPieces = new GameObject("GroundPieces");
         for (int i = -10; i < groundLength; i++)
         {
             GameObject piece = GameObject.Instantiate(groundPrefab, new Vector3(i*5.0f, -7.5f, 0.0f), Quaternion.identity) as GameObject;
             piece.transform.SetParent(groundPieces.transform);
+        }
+        //background
+        GameObject backgrounds = new GameObject("Backgrounds");
+        for (int i = -1; i < groundLength / 5; i++)
+        {
+            GameObject back = GameObject.Instantiate(backgroundImage, new Vector3(i * 50, -2.25f, 1f), Quaternion.identity) as GameObject;
+            back.transform.SetParent(backgrounds.transform);
         }
     }
 
